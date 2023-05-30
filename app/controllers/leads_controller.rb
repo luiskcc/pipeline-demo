6783b66e9c928ef5943ad62c675335bbe92dc43e
“luiskcc” <114909465+luiskcc@users.noreply.github.com>
2023-05-30 14:11:09 +0100

    update_stage_without_creating_new action in model

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -6,7 +6,7 @@ class LeadsController < ApplicationController
   #PUT /leads/:id/update_stage
   def update_stage
     @lead = Lead.find(params[:id])
-    @lead.update(stage: params[:stage_id])
+    @lead.update_stage_without_creating_new(stage: params[:stage_id])
     render json: {success: true}
   end
 
--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,2 +1,7 @@
 class Lead < ApplicationRecord
+
+  def update_stage_without_creating_new(new_stage)
+    self.update_columns(stage: new_stage)
+  end
+
 end
